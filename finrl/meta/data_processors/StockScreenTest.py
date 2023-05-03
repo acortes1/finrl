78e43c8b0c0d2c4a9c15f82cc1ac389525c11570
acortes1 <albertcortesjr@gmail.com>
2023-05-02 22:55:56 -0700

    Update StockScreenTest.py

--- finrl/meta/data_processors/StockScreenTest.py
+++ finrl/meta/data_processors/StockScreenTest.py
@@ -1,1 +1,34 @@
+import yfinance as yf
+import pandas as pd
+import datetime
+import time
+import schedule
 
+def get_best_stocks():
+    # Define the pool of stocks for swing trading
+    stock_list = ['AAPL', 'MSFT', 'GOOGL', 'AMZN', 'FB', 'TSLA', 'NFLX', 'NVDA', 'PYPL']
+
+    # Define the indicators to screen stocks
+    indicators = {
+        'previous_close': [],
+        'MovingAverage_10': [],
+        'RSI_14': [],
+        'volume': []
+    }
+
+    # Add your own calculations for the above indicators here
+    # For example, use yfinance to get historical data for each stock and calculate the indicators
+
+    # Add dummy data for demonstration purposes
+    for stock in stock_list:
+        indicators['previous_close'].append(stock + '_previous_close')
+        indicators['MovingAverage_10'].append(stock + '_MOV10')
+        indicators['RSI_14'].append(stock + '_RSI14')
+        indicators['volume'].append(stock + '_volume')
+
+    return pd.DataFrame(indicators, index=stock_list)
+
+def rescreen_stocks():
+    print("Re-screening stocks...")
+    best_stocks = get_best_stocks()
+    print(best_stocks)
